8b050124c1846caa603a3d8a190d82b3c00e0819
nllew <n.lewis.perc@gmail.com>
2021-05-24 13:56:10 -0500

    added tip buttons and input field

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            inputValue: "",
+            tip: 0.2,
+        };
+    }
+
+    updateCustomTip(text) {
+        if (text.customTip) {
+            this.setState({ tip: parseFloat(text.customTip) / 100 });
+        } else {
+            this.setState({ tip: 0 });
+        }
+    }
+
+    render() {
+        let tip = 0.0;
+        if (this.state.inputValue) {
+            tip = parseFloat(this.state.inputValue) * this.state.tip;
+            tip = (Math.round(tip * 100) / 100).toFixed(2);
+        }
+
+        return (
+            <View style={styles.container}>
+                <Text>${tip}</Text>
+                <TextInput
+                    onChangeText={(text) => this.setState({ inputValue: text })}
+                    style={styles.input}
+                    keyboardType="numeric"
+                    placeholder="0.00"
+                    value={this.state.inputValue}
+                />
+                <View style={styles.buttonGroup}>
+                    <Button
+                        title="10%"
+                        onPress={() => this.setState({ tip: 0.1 })}
+                    />
+                    <Button
+                        title="20%"
+                        onPress={() => this.setState({ tip: 0.2 })}
+                    />
+                    <Button
+                        title="25%"
+                        onPress={() => this.setState({ tip: 0.25 })}
+                    />
+                    <TextInput
+                        value={(this.state.tip * 100).toString()}
+                        style={styles.customTip}
+                        onChangeText={(customTip) =>
+                            this.updateCustomTip({ customTip })
+                        }
+                        keyboardType="numeric"
+                        placeholder="0.00"
+                    />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    input: {
+        height: 40,
+        width: "100%",
+        borderColor: "#333",
+        borderWidth: 1,
+        padding: 5,
+    },
+    buttonGroup: {
+        flexDirection: "row",
+        margin: 5,
+    },
+    customTip: {
+        height: 30,
+        width: 60,
+        borderColor: "#333",
+        borderWidth: 1,
+        padding: 5,
+    },
 });
