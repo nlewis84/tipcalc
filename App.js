cf6bb71736c811aa66d5b67da681c20cce26706a
nllew <n.lewis.perc@gmail.com>
2021-05-24 21:22:50 -0500

    completed app

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Button, StyleSheet, Text, TextInput, View, Alert } from "react-native";
-import Head from "./ui/Head";
+import { Button, StyleSheet, TextInput, View, Alert } from "react-native";
 import Values from "./ui/Values";
 
 export default class App extends React.Component {
@@ -22,46 +21,47 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <View>
-                <View>
-                    <Head />
-                </View>
-                <View style={styles.container}>
+            <View style={styles.container}>
+                <View style={{ width: "100%" }}>
                     <Values
                         tipPercent={this.state.tip}
                         bill={this.state.inputValue}
                     />
-                    <TextInput
-                        onChangeText={(text) =>
-                            this.setState({ inputValue: text })
-                        }
-                        style={styles.input}
-                        keyboardType="numeric"
-                        placeholder="0.00"
-                        value={this.state.inputValue}
-                    />
-                    <View style={styles.buttonGroup}>
-                        <Button
-                            title="10%"
-                            onPress={() => this.setState({ tip: 0.1 })}
-                        />
-                        <Button
-                            title="20%"
-                            onPress={() => this.setState({ tip: 0.2 })}
-                        />
-                        <Button
-                            title="25%"
-                            onPress={() => this.setState({ tip: 0.25 })}
-                        />
+                    <View style={styles.inputs}>
                         <TextInput
-                            value={(this.state.tip * 100).toString()}
-                            style={styles.customTip}
-                            onChangeText={(customTip) =>
-                                this.updateCustomTip({ customTip })
+                            onChangeText={(text) =>
+                                this.setState({ inputValue: text })
                             }
+                            style={styles.input}
                             keyboardType="numeric"
                             placeholder="0.00"
+                            placeholderTextColor="#FFF"
+                            value={this.state.inputValue}
                         />
+                        <View style={styles.buttonGroup}>
+                            <Button
+                                title="10%"
+                                onPress={() => this.setState({ tip: 0.1 })}
+                            />
+                            <Button
+                                title="20%"
+                                onPress={() => this.setState({ tip: 0.2 })}
+                            />
+                            <Button
+                                title="25%"
+                                onPress={() => this.setState({ tip: 0.25 })}
+                            />
+                            <TextInput
+                                value={(this.state.tip * 100).toString()}
+                                style={styles.customTip}
+                                onChangeText={(customTip) =>
+                                    this.updateCustomTip({ customTip })
+                                }
+                                keyboardType="numeric"
+                                placeholder="0.00"
+                                placeholderTextColor="#FFF"
+                            />
+                        </View>
                     </View>
                 </View>
             </View>
@@ -72,27 +72,32 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#000",
         alignItems: "center",
         justifyContent: "center",
+        padding: 20,
+        height: "100%",
+        width: "100%",
+    },
+    inputs: {
+        backgroundColor: "#212121",
         padding: 20,
     },
     input: {
         height: 40,
         width: "100%",
-        borderColor: "#333",
-        borderWidth: 1,
         padding: 5,
+        color: "#FFF",
     },
     buttonGroup: {
         flexDirection: "row",
         margin: 5,
+        justifyContent: "space-between",
     },
     customTip: {
-        height: 30,
+        height: 40,
         width: 60,
-        borderColor: "#333",
-        borderWidth: 1,
         padding: 5,
+        color: "#FFF",
     },
 });
--- ui/Values.js
+++ ui/Values.js
@@ -3,16 +3,44 @@ import { StyleSheet, Text, View } from "react-native";
 
 const Values = ({ tipPercent, bill }) => {
     let tip = 0.0;
+    let total = 0.0;
     if (bill) {
         tip = parseFloat(bill) * tipPercent;
+        total = parseFloat(bill) + tip;
         tip = (Math.round(tip * 100) / 100).toFixed(2);
+        total = (Math.round(total * 100) / 100).toFixed(2);
     }
 
     return (
-        <View>
-            <Text>${tip}</Text>
+        <View style={styles.values}>
+            <Text style={styles.label}>Tip Amount</Text>
+            <Text style={styles.tip}>${tip}</Text>
+            <Text style={styles.label}>Total Amount</Text>
+            <Text style={styles.total}>${total}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    values: {
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: "#484848",
+        width: "100%",
+    },
+    label: {
+        color: "#FFF",
+    },
+    tip: {
+        fontSize: 60,
+        color: "#FFF",
+        fontWeight: "bold",
+    },
+    total: {
+        fontSize: 30,
+        color: "#FFF",
+        fontWeight: "bold",
+    },
+});
 
 export default Values;
